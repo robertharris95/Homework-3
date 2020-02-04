e57756f186e50617eaa53b33438b2be4eecfcacf
Robert Harris <rlh9@rice.edu>
2020-02-04 09:30:12 -0600

    Edits as of 2/3/20

--- Develop/script.js
+++ Develop/script.js
@@ -15,9 +15,13 @@ var character = ["!" , "#" , "$" , "%" , "&" , "(" , ")" , "*" , "+" , "-" , "."
 // array of all numbers
 var number = [1,2,3,4,5,6,7,8,9,0];
 // array of all UPPERCASE letters
-var upper =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var upper =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];  
 // array of all lowercase letters
-var lower = upper.toLowercase();
+var lower =  ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// for({
+
+// }
