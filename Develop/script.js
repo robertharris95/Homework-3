042704fd4bf29fde8eb283c95b5b1394be2ae18c
Robert Harris <rlh9@rice.edu>
2020-02-05 09:37:30 -0600

    Created randomizers to generate a random character

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,9 @@
-// Assignment Code
+// ASSIGNMENT CODE
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -9,19 +11,53 @@ function writePassword() {
   passwordText.value = password;
 
 }
-// Var arrays
-// array of all special characters
-var character = ["!" , "#" , "$" , "%" , "&" , "(" , ")" , "*" , "+" , "-" , "." , "/" , ":" , ";" , "<" , "=" , ">" , "?" , "@" , "[" , "]" , "^" , "_" , "`" , "{" , "|" , "}" , "~"];
-// array of all numbers
-var number = [1,2,3,4,5,6,7,8,9,0];
-// array of all UPPERCASE letters
-var upper =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];  
-// array of all lowercase letters
-var lower =  ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// for({
 
-// }
+
+// STUDENT CODE
+
+// Var arrays
+
+// array of all special characters
+var character = ["!" , "#" , "$" , "%" , "&" , "(" , ")" , "*" , "+" , "-" , "." , "/" , ":" , ";" , "<" , "=" , ">" , "?" , "@" , "[" , "]" , "^" , "_" , "`" , "{" , "|" , "}" , "~"];
+
+// array of all UPPERCASE letters
+var upper =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];  
+
+// array of all lowercase letters
+var lower =  ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+
+//array of all
+var possible = [randlower, randnum, randsymbol, randupper]
+
+
+// Randomizers
+
+// Numbers
+// create minimum and maximum values
+var min = Math.floor(0);
+var max = Math.ceil(9);
+
+// Generate a random number between 0 and 9
+var randnum = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// Uppercase Letters
+var randupper = upper[Math.floor(Math.random() * upper.length)];
+
+// lowercase letters
+var randlower = lower[Math.floor(Math.random() * lower.length)];
+
+// special characters
+var randsymbol = character[Math.floor(Math.random() * character.length)];
+
+//Random of Random elements for Final Display
+var random = possible[Math.floor(Math.random() * possible.length)];
+
+// Password Generation
+
+
+//Password Display
+ var password = document.getElementbbyId(password);
+password.innerHTML = 
